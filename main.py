6547d8042e7ccc1bdf3b54fb06691aee2352f5f5
barbaragrcast <barbaragrcast@gmail.com>
2026-04-18 14:57:13 -0500

    s

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("HELLO - SCRIPT IS RUNNING")
+import mysql.connector
+
+print("Starting program...")
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Chispita2013*",   # your password
+    database="banking_app"      # MUST match your DB
+)
+
+cursor = conn.cursor()
+
+print("Connected!")
+
+# Query your table (CHANGE NAME IF NEEDED)
+cursor.execute("SELECT * FROM bank_accounts")  # <-- change if your table name is different
+
+rows = cursor.fetchall()
+
+print("Rows:", rows)
+
+for row in rows:
+    print(row)
+
+conn.close()
+
+print("Done.")
