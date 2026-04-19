21d682c1c85f77d3e388c3a4c40ded5e5c1ae7ef
barbaragrcast <barbaragrcast@gmail.com>
2026-04-19 12:34:15 -0500

    e

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # loads .env file
-
-def get_connection():
-    conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
-    return conn
+from db import get_connection
 
 print("Starting program...")
 
@@ -26,6 +13,7 @@ print("Connected!")
 cursor.execute("SELECT * FROM bank_accounts")  # <-- change if your table name is different
 
 rows = cursor.fetchall()
+
 
 
 for row in rows:
