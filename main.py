27729741e0f9e553cc00117fef35cdf2c86b1bb5
barbaragrcast <barbaragrcast@gmail.com>
2026-04-19 12:26:46 -0500

    ss

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from db import get_connection
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env file
+
+def get_connection():
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    return conn
 
 print("Starting program...")
 
